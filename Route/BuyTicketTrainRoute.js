e7bb6d55cf0b55ec98ec01e5b401e68884eaeed2
quan3473 <nguyenminhquan042004@gmail.com>
2024-09-01 19:46:03 +0700

    backup new repo

---
+++ Route/BuyTicketTrainRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const route = express.Router();
+
+const {
+  GetPhieusdattau,
+  BuyTicketTrain,
+  SchedularChange,
+  CancelTicketTrain,
+  FindBuyTicketTrainMaDX,
+} = require("../Controller/BuyTicketTrainController.js");
+
+route.post("/GetPhieusdattau", GetPhieusdattau);
+route.post("/BuyTicketTrain", BuyTicketTrain);
+route.get("/FindBuyTicketTrainMaDX/:MaVeTau", FindBuyTicketTrainMaDX);
+route.put("/SchedularChange/:id", SchedularChange);
+route.delete("/CancelTicketTrain/:MaDX", CancelTicketTrain);
+
+module.exports = route;
